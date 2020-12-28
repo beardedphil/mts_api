bca673f084121efd3e5a7e166f26f3ac588dec8a
Phil King <phil@beardedphil.com>
2020-12-28 11:14:15 -0600

    Added Articles model to admin area. Updated model.

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    source = models.CharField(max_length=25)
+    article_link = models.URLField()
+    image_link = models.URLField()
+    pub_date = models.DateField()
+    keywords = ArrayField(
+        models.CharField(max_length=50),
+        blank=True
+    )
+
+    def __str__(self):
+        return self.title
